4aefb6163a36efeeda552c5bd68c18bef13cb9b9
Faris Chtatou <faris.manage@gmail.com>
2026-01-12 16:29:42 +0100

    Fix pdf indexing issue

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/Faris_Chtatou_Resume.pdf",
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: "index, noarchive",
+          },
+          {
+            key: "Content-Type",
+            value: "application/pdf",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
